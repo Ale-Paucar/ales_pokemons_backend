a192e4e841d126a5fab5157a912e4cd8a68ceeb9
Ale <nuala.ni.ale@gmail.com>
2023-08-29 11:22:38 -0500

    prueba conexion db

--- src/db.js
+++ src/db.js
@@ -4,13 +4,19 @@ const fs = require('fs');
 const path = require('path');
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASENAME } = process.env;
 
-const sequelize = new Sequelize(
-   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASENAME}`,
-   {
-      logging: false, // set to console.log to see the raw SQL queries
-      native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+const sequelize = new Sequelize(DB_DATABASENAME, DB_USER, DB_PASSWORD, {
+   host: DB_HOST,
+   dialect: 'postgres',
+   // mostrara cada ves que se levante el servidor la respuesta de la base de datos mientras 'MODE' sea distinto de 'PRODUCTION'
+   logging: false,
+   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+   dialectOptions: {
+      ssl: {
+         require: true
+      }
    }
-);
+})
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
